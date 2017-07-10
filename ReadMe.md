e78bdf583debe1d73a08ff983d15e00c64356e8d
ESENDEX\scott.lyons <scott.lyons@esendex.com>
2017-07-10 11:25:27 +0100

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,15 @@
+# ASP.NET Core using Angular
+This is an example application of an angular front end using a ASP.NET Core backend service
+
+## How to run
+First ensure that you have the angular-cli installed using 
+
+`npm install @angular/cli --global`
+
+Next run the following commands to build the application.
+
+`$ cd .\src\Web\`  
+`$ npm install`  
+`$ ng build`  
+
+Finally open the visual studio solution and run the application.
---
+++ src/Web/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                    !Path.HasExtension(context.Request.Path.Value) &&
+                    !context.Request.Path.Value.StartsWith("/api"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
